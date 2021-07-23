6b08b25a8d2c9ccb5f79e1c378d36174348d4385
Mr Cobiro <87790524+MRCOBIRO@users.noreply.github.com>
2021-07-23 09:18:20 +0700

    Add files via upload

---
+++ chanell.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# tampilan
+clear
+echo
+toilet -f big -F gay Youtube
+echo -e "============================================================" | lolcat
+echo -e " Author : Mr Zamibro" | lolcat
+echo -e " Team   : Mafia Teknologi Indonesia " | lolcat
+echo -e "============================================================" | lolcat
+echo
+echo -e "Saya Akan Membagikan Chanel Youtube Hacker" | lolcat
+echo
+echo -e "[1]. Mr Virus Spm" | lolcat
+echo -e "[2]. Panglima Jateng" | lolcat
+echo -e "[3]. Mafia Teknologi" | lolcat
+echo -e "[4]. PapiKing21" | lolcat
+echo -e "[5]. Mr A.M" | lolcat
+echo -e "[6]. Scrpit Kiddie" | lolcat
+echo -e "[7]. MR Ibat999" | lolcat
+echo -e "[8]. Mr.Syahrul" | lolcat
+echo -e "[9]. Mr.Zamibro" | lolcat
+echo -e "[10]. CyberTeam001" | lolcat
+echo -e "[11]. Anker Production" | lolcat
+echo -e "[12]. Anonymous Cyber I.N.A" | lolcat
+echo -e "[13]. Tn Draco" | lolcat
+echo -e "[14]. Mr Cobiro" | lolcat
+read -p "pilih: " mana
+
+if [ $mana = 1 ]
+then
+    xdg-open https://youtube.com/channel/UCplRguzksGT4x9TPdf9xBBQ
+fi
+
+if [ $mana = 2 ]
+then
+    xdg-open https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+fi
+
+if [ $mana = 3 ]
+then
+    xdg-open https://youtube.com/channel/UCIEWrGcR1N4v4eIkVh3mX8Q
+fi
+
+if [ $mana = 4 ]
+then
+    xdg-open https://youtube.com/channel/UClBmb0PBsTmCFNwkPz64NFg
+fi
+
+if [ $mana = 5 ]
+then
+    xdg-open https://youtube.com/channel/UCXk4vbvDl7i6dxWBtNdpx6w
+fi
+
+if [ $mana = 6 ]
+then
+    xdg-open https://youtube.com/channel/UCIPpb1Zku_ulifWef0d_Y2Q
+fi
+
+if [ $mana = 7 ]
+then
+    xdg-open https://youtube.com/c/IrsyadGaming1235363youtubechenel
+fi
+
+if [ $mana = 8 ]
+then
+    xdg-open https://youtube.com/channel/UCblgK_Xr14FlcPV5b2V1Jag
+fi
+
+if [ $mana = 9 ]
+then
+    xdg-open https://youtube.com/channel/UCyDbhmh5zvheFI8A7u4vaLg
+fi
+
+if [ $mana = 10 ]
+then
+    xdg-open https://youtube.com/channel/UC4a22qPTvnFNtnGcg0N9FKw
+fi
+
+if [ $mana = 11 ]
+then
+    xdg-open https://youtube.com/c/AnkerProduction
+fi
+
+if [ $mana = 12 ]
+then
+    xdg-open https://youtube.com/channel/UC88izQKltz49PE8Z4I8dVCQ
+fi
+
+if [ $mana = 13 ]
+then
+    xdg-open https://youtube.com/channel/UCEwF4EaxX_MvCAziV2IO-qg
+fi
+
+if [ $mana = 14 ]
+then
+    xdg-open https://youtube.com/channel/UC2F_TFPuynm51OYUmiLnz0g
+fi
+
